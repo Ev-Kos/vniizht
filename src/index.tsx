bb583fa57e1c2ed3db67bd22d51d7917d9b93921
Evgeniya Kostyrya <zhenya241191@gmail.com>
2023-08-29 21:34:30 +0300

    gh-page

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={ store }>
-    <BrowserRouter>
+    <BrowserRouter basename='/vniizht'>
         <App />
       </BrowserRouter>
     </Provider>
